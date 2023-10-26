e1683e3a839e32b4804b8c89c0f2bb98b87d4381
bab882 <chanitebrandon@gmail.com>
2023-10-26 22:40:35 +0200

    debug

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
     smooth: true,
     tablet: {smooth: true },
+    mobile: {smooth: true},
 });
 
 // Body color
@@ -20,23 +21,21 @@ const bulle = document.createElement("div");
 bulle.classList.add('bulle');
 document.body.appendChild(bulle);
 
-// Rendre la bulle visible et donner des coordonnées au curseur
 const circleBehaviour = (e) => {
     bulle.style.display = "block" 
     bulle.style.opacity = 1
     bulle.style.transform = `translateX(${e.pageX}px) translateY(${e.pageY}px)`
     bulle.style.transition = "0.15s"
 }
-// J'ajoute un ecouteur d'événement dans le DOM
-// Pour Déclencher la fonction circleBehaviour ( maj des styles de la bulle en fonction de la position de la souris)
+
 document.addEventListener("mousemove", (e) => {
     circleBehaviour(e)
 });
-// J'ajoute un evenement pour rendre la souris invisble
+
 document.addEventListener("mouseout", (e) => {
     bulle.style.opacity = 0
 });
-// Pour que la bulle remonte au scroll
+
 document.addEventListener("wheel", (e) => {
     bulle.style.display = "block"
     bulle.style.opacity = 1
@@ -47,4 +46,3 @@ function activateIcon(id) {
     var icon = document.getElementById("icon" + id.substring(4));
     icon.classList.add("active");
 }
-//  End Curseur de la souris
