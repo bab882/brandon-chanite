8d9458b64a93498481d55975a1f89803e1fc9f6d
bab882 <chanitebrandon@gmail.com>
2023-10-26 22:54:33 +0200

    debug

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 const scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
     smooth: true,
-    tablet: {smooth: true },
-    smartphone: { smooth: true },
+    tablet: { smooth: true },
+    smartphone: { smooth: true }
 });
 
 // Body color
@@ -16,33 +16,33 @@ scroll.on('scroll', () => {
     }
 });
 
-// // Curseur de la souris
-// const bulle = document.createElement("div");
-// bulle.classList.add('bulle');
-// document.body.appendChild(bulle);
+// Cursor
+const bulle = document.createElement("div");
+bulle.classList.add('bulle');
+document.body.appendChild(bulle);
 
-// const circleBehaviour = (e) => {
-//     bulle.style.display = "block" 
-//     bulle.style.opacity = 1
-//     bulle.style.transform = `translateX(${e.pageX}px) translateY(${e.pageY}px)`
-//     bulle.style.transition = "0.15s"
-// }
+const circleBehaviour = (e) => {
+    bulle.style.display = "block" 
+    bulle.style.opacity = 1
+    bulle.style.transform = `translateX(${e.pageX}px) translateY(${e.pageY}px)`
+    bulle.style.transition = "0.15s"
+}
 
-// document.addEventListener("mousemove", (e) => {
-//     circleBehaviour(e)
-// });
+document.addEventListener("mousemove", (e) => {
+    circleBehaviour(e)
+});
 
-// document.addEventListener("mouseout", (e) => {
-//     bulle.style.opacity = 0
-// });
+document.addEventListener("mouseout", (e) => {
+    bulle.style.opacity = 0
+});
 
-// document.addEventListener("wheel", (e) => {
-//     bulle.style.display = "block"
-//     bulle.style.opacity = 1
-//     bulle.style.transform = `translateX(${e.pageX}px) translateY(${e.pageY}px)`
-//     bulle.style.transition = "0.15s"
-// });
-// function activateIcon(id) {
-//     var icon = document.getElementById("icon" + id.substring(4));
-//     icon.classList.add("active");
-// }
+document.addEventListener("wheel", (e) => {
+    bulle.style.display = "block"
+    bulle.style.opacity = 1
+    bulle.style.transform = `translateX(${e.pageX}px) translateY(${e.pageY}px)`
+    bulle.style.transition = "0.15s"
+});
+function activateIcon(id) {
+    var icon = document.getElementById("icon" + id.substring(4));
+    icon.classList.add("active");
+}
